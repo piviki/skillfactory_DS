0300f74a5b00345efe051af9c6746f66f94505bf
piviki <piviki@mail.ru>
2022-03-24 11:30:45 +0300

    --- Auto-Git Commit ---

---
+++ counter_collections_mode.py
@@ -0,0 +1,17 @@
+# Counter ()
+
+# Creating empty Counter
+from collections import Counter
+c = Counter()
+c['red'] +=1
+print('Adding red car to Counter', c)
+# Adding red car to Counter Counter({'red': 1})
+
+# Creating a list of cars
+from collections import Counter
+cars = ['red', 'red', 'black', 'white', 'green', 'yellow', 'white']
+c = Counter()
+for car in cars:
+    c[car] +=1
+print('Making dict for the car list', c)
+# Making dict for the car list Counter({'red': 2, 'white': 2, 'black': 1, 'green': 1, 'yellow': 1})
