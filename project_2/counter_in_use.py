3558e1d649c86a73f18f456b9478b4f7706fcc93
piviki <piviki@mail.ru>
2022-03-24 12:49:39 +0300

    --- Auto-Git Commit ---

--- project_2/counter_in_use.py
+++ project_2/counter_in_use.py
@@ -1,11 +1,30 @@
-# Counter use
-
-# Creat counters
+# counter in use
 from collections import Counter
+cars_list_moscow = ['blue', 'red', 'white', 'red', 'green', 'orange', 'purple', 'white', 'white']
+cars_list_spb = ['white', 'white', 'blue', 'red', 'funny', 'purple']
+# print(cars_list_moscow)
+moscow_counter = Counter(cars_list_moscow)
+spb_counter = Counter(cars_list_spb)
+print('Moscow', moscow_counter)
+print('SPb', spb_counter)
+# Moscow Counter({'white': 3, 'red': 2, 'blue': 1, 'green': 1, 'orange': 1, 'purple': 1})
+# SPb Counter({'white': 2, 'blue': 1, 'red': 1, 'funny': 1, 'purple': 1})
 
-moscow_list = ['red', 'white', 'white', 'green', 'black', 'purple']
-spb_list = ['white', 'green','yellow', 'purple', 'white', 'green']
-moscow_count = Counter(moscow_list)
-spb_counter = Counter(spb_list)
-print('Moscow counter', moscow_count)
-print('Spb counter', spb_counter)
+# another way
+c = Counter()
+b = Counter()
+for car in cars_list_moscow:
+    c[car] +=1
+for car_num in cars_list_spb:
+    b[car_num] +=1
+print('Moscow', c)
+print('SPb', b)
+# Moscow Counter({'white': 3, 'red': 2, 'blue': 1, 'green': 1, 'orange': 1, 'purple': 1})
+# SPb Counter({'white': 2, 'blue': 1, 'red': 1, 'funny': 1, 'purple': 1})
+
+# elemets()
+print('Moscow:', *moscow_counter.elements())
+# Moscow: blue red red white white white green orange purple
+print('SPb:', *spb_counter)
+# SPb: white blue red funny purple
+
