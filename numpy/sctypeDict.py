72ebaa74dbb8d5e23b2901f80aaec53fdebf29f3
piviki <piviki@mail.ru>
2022-03-28 13:24:41 +0300

    --- Auto-Git Commit ---

--- numpy/sctypeDict.py
+++ numpy/sctypeDict.py
@@ -1,2 +1,8 @@
 # sctypeDict = словарь типов данных в NumPy 
-# 
+# в этом словаре содержится более 100 ключей
+import numpy as np
+print(np.sctypeDict)
+print(len(np.sctypeDict))
+
+# Получить список названий уникальных типов
+print(*sorted(map(str, set(np.sctypeDict.values()))), sep='\n')
