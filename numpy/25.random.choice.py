4a3f4698224656275352f0b84213ff320faae37d
piviki <piviki@mail.ru>
2022-03-31 16:45:28 +0300

    --- Auto-Git Commit ---

--- numpy/25.random.choice.py
+++ numpy/25.random.choice.py
@@ -7,4 +7,15 @@
 import numpy as np
 # Выберем случайным образом из списка двоих человек, которые должны будут выступить с отчётом на этой неделе
 workers = ['Ivan', 'Nikita', 'Maria', 'John', 'Kate']
-choice = np.
+choice = np.random.choice(workers, size=2, replace=False)
+print(choice)
+# ['Ivan' 'Maria']
+choice2 = np.random.choice(workers)
+print(choice2)
+# Kate # возвращается один объект по умолчанию
+
+# получим случайную последовательность, которая образуется в результате десяти подбрасываний игральной кости
+coin_choice = np.random.choice([1,2,3,4,5,6], size=10)
+print(coin_choice)
+# [4 4 5 1 5 1 5 1 5 2] 
+# ошибка не возникает за счёт того, что объекты могут повторяться
