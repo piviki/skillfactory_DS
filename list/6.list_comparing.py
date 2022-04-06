109849e27c9f0cade8101e8db97794f513f3224e
piviki <piviki@mail.ru>
2022-04-06 12:33:54 +0300

    --- Auto-Git Commit ---

--- list/6.list_comparing.py
+++ list/6.list_comparing.py
@@ -1,2 +1,9 @@
 # Сравнение списков
-# два списка считаются равными, если они содержат один и тот же 
+# два списка считаются равными, если они содержат один и тот же набор элементов
+numbers = [1,2,3,4,5,6,7,8,9]
+numbers2 =list(range(1,10))
+if numbers == numbers2:
+    print('numbers equal to numbers')
+else:
+    print('numbers is not equal to numbers')
+# numbers equal to numbers
